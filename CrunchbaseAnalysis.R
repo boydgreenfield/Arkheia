a0c91d2669eb086ce4292c113bf2b5adb0120e1f
Nick Greenfield <nick.greenfield@gmail.com>
2010-12-25 13:33:56 -0500

    Added a few lines of code from the spatial analysis work. Still experimenting with Git.

--- CrunchbaseAnalysis.R
+++ CrunchbaseAnalysis.R
@@ -1,5 +1,16 @@
+# File-Name:       CrunchBaseAnalysis.R
+# Date:            2010-12-24                                
+# Author:          Nick Greenfield
+# Purpose:         Brief visualization of tech start-ups by state
+# Data Used:       2004_Election_Counties.shp
+# Packages Used:   spdep
+# Output File:     
+# Data Output:     
+# Machine:         Nick Greenfield's MacBook Pro
+
 # Load prerequisite libraries
-library(ggplot2)
+gpclibPermit()
+library(spdep)
 
 # Bring in the Crunchbase database, get its dimensions and header names
 cb <- read.csv("/Personal/Data/Crunchbase/companydata.csv",header=TRUE)
@@ -7,4 +18,5 @@ colnames <- names(cb)
 nrows <- dim(cb)[1]
 ncols <- dim(cb)[2]
 
-test.object <- c(1,2,3)
+# Add a shapepoly of the US
+USA <- readShapePoly("/Personal/Arkheia/2004_Election_Counties/2004_Election_Counties.shp")
